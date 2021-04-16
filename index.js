a0871e597f2d4625421e06f1f16db2ddecfbded3
Asrorjon <asrorjon.axrorovich@gmail.com>
2021-04-16 13:39:42 +0500

    Asrorjon

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require('express')
-const port = process.env.port || 1111
-const md5 = require('md5')
+const port = process.env.port || 8080
 const app = express()
-const db = require('./db')
-const usctrl = require('./user_controller')
+const approuter = require('./router/approuter')
 //parse HTML formm
 app.use(express.urlencoded())
 app.set('view engine', 'ejs')
@@ -12,38 +10,6 @@ app.use(express.static('signup'))
 app.use(express.static('login'))
 app.use(express.static('views/profile'))
 app.set('view engine', 'ejs')
-
-app.get('/signup', function (req,res){
-    res.sendFile(__dirname+'/signup/signup.html')
-})
-
-app.get('/login', function (req,res){
-    res.sendFile(__dirname+'/login/login.html')
-})
-
-// registratiom
-app.post('/signup', function (req, res) {
-    usctrl.createUser(req,res)
-})
-
-//login
-app.post('/login', function (request, response) {
-    email = request.body.user.email
-    password = request.body.user.password
-        console.log(email)
-
-    const query = {
-        // give the query a unique name
-        name: 'fetch-user',
-        text: 'SELECT * FROM person WHERE email = $1 AND password = $2',
-        values: [email, md5(password)],
-    }
-
-    db.query(query, function (err, res) {
-        if (err) console.log(err)
-        console.log(res)
-        response.render('profile/profile', {last: res.rows[0].last, first: res.rows[0].first})
-        })
-})
+app.use('/', approuter)
 
 app.listen(port, () => console.log('server started on post ' + port))
--- router.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const userController = require('./user_controller')
-
-router.post('/signup', userController.createUser)
-router.post('/login', userController.getUser)
-
-module.exports = router
